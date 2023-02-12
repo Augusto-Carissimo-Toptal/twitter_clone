8ca8b5688a8ea67c7039eb7821a29437bfdf9968
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-02-12 20:14:35 -0300

    Add User, Tweet, Like models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
+  has_many :likes
+  has_many :liked_by, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets
   has_many :likes
-  has_many :liked_tweet, through: likes, source: :tweets
+  has_many :liked_tweet, through: :likes, source: :tweet
+
+
 end
