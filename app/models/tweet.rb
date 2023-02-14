955d67531127555bf76adfb1f84651b1791774e2
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-02-13 22:28:34 -0300

    Add dependent: :destroy to all models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,6 @@ class Tweet < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_by, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
 
-  has_many :tweets
-  has_many :likes
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_tweet, through: :likes, source: :tweet
 end
